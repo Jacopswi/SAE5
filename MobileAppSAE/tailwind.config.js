07d599a7ccabd8f295f550608f1d0d54850b42d2
unknown <jacopswi54@gmail.com>
2024-10-22 18:24:46 +0200

    ajout tailwind + css partie login

---
+++ MobileAppSAE/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts,tsx}',
+    './src/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
